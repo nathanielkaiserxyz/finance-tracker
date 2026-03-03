75677486243a91b9c17ddea38ee8545d1db82aba
Nathaniel Kaiser <kaisernathaniel@gmail.com>
2026-03-02 21:15:45 -0600

    initial

---
+++ input.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int week;
+    float rent;
+    float internet;
+    float electric;
+    float rentinsurance;
+    float groceries;
+    float 
+    float gas;
+    /**/
+    printf("enter amount for gas: ");
+    
+    scanf("%f", &gas);
+    
+    /**/
+    printf("gas price:: %.2f°C\n", gas);
+    return 0;
+}
+
